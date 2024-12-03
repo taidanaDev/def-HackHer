dce411a36fb29527a532319bcca44f6ea0137159
taidanaDev <bandiezshekinah18@gmail.com>
2024-12-03 16:24:42 +0800

    Update auth.py

--- WEB APP/website/auth.py
+++ WEB APP/website/auth.py
@@ -39,27 +39,28 @@ def sign_up():
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
+
         if user:
             flash('Email already exists.', category='error')
         elif db.session.query(User).filter_by(username=username).first() is not None:
             flash('Username already exists.', category='error')
-        elif len(email) < 4:
+        elif not email or len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif len(username) < 3:
+        elif not name or len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+        elif not username or len(username) < 3:
             flash('Username must be greater than 2 characters.', category='error')
-        elif password1 != password2:
+        elif not password1 or not password2 or password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
+        elif not password1 or len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, username=username, first_name=first_name,
+            new_user = User(email=email, username=username, name=name,
                             password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
